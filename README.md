6ddf3b78e2790a44b695c48050afeac49a0dc0c6
MockingDev <mockingdev@gmail.com>
2022-01-13 20:21:37 +0100

    proyect update

---
+++ README.md
@@ -0,0 +1,15 @@
+# How-To EXPRESS-LOCALLIBRARY-TUTORIAL
+
+> Puedes encontrar la version ORIGINAL completamente desarrollada [aqui](https://github.com/mdn/express-locallibrary-tutorial).
+
+## Requisitos
+
+* node y npm
+* express
+* express-generator
+* nodemon
+* mongoose
+* async
+* luxon
+* morgan
+* pug
---
+++ models/bookinstance.js
@@ -0,0 +1,33 @@
+const { DateTime } = require("luxon");
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var BookInstanceSchema = new Schema({
+    book: { type: Schema.Types.ObjectId, ref: 'Book', required: true }, //reference to the associated book
+    imprint: { type: String, required: true },
+    status: { type: String, required: true, enum: ['Available', 'Maintenance', 'Loaned', 'Reserved'], default: 'Maintenance' },
+    due_back: { type: Date, default: Date.now }
+});
+
+// Virtual for bookinstance's URL
+BookInstanceSchema
+    .virtual('url')
+    .get(function() {
+        return '/catalog/bookinstance/' + this._id;
+    });
+
+BookInstanceSchema
+    .virtual('due_back_formatted')
+    .get(function() {
+        return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
+    });
+
+BookInstanceSchema
+    .virtual('due_back_html')
+    .get(function() {
+        return DateTime.fromJSDate(this.due_back).toFormat('yyyy-MM-dd');
+    });
+
+//Export model
+module.exports = mongoose.model('BookInstance', BookInstanceSchema);
