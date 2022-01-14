f198234867bfa2a9907d8547974eb16d8925de84
MockingDev <mockingdev@gmail.com>
2022-01-14 16:41:21 +0100

    Include Compression and Helmet Middleware

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var compression = require('compression');
+var helmet = require('helmet')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -10,12 +12,17 @@ var catalogRouter = require('./routes/catalog');
 
 var app = express();
 
+app.use(helmet());
+
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://fullstack:FullSt4ck@cluster0.hzrpt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://fullstack:FullSt4ck@cluster0.hzrpt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use(compression()); //Compress all routes
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
